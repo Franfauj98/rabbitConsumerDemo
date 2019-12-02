dd910a1aa7849b24194fdde8498632a2f253c4fe
Franfauj98 <francois.faujeau@gmail.com>
2019-12-02 20:18:58 +0100

    RabbitConfig

--- src/main/java/com/rabbit/consumer/rabbitconsumerdemo/RabbitConsumerDemoApplication.java
+++ src/main/java/com/rabbit/consumer/rabbitconsumerdemo/RabbitConsumerDemoApplication.java
@@ -3,9 +3,11 @@ package com.rabbit.consumer.rabbitconsumerdemo;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @EnableRabbit
 @SpringBootApplication
+@EntityScan(basePackages = {"com.rabbit.producer.rabbitconsumerdemo"})
 public class RabbitConsumerDemoApplication {
 
 	public static void main(String[] args) {
